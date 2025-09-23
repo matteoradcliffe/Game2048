93969f98030bbb5bbb028f36361878e4ec81e1e9
Matteo Radcliffe <mdradcliffe@wm.edu>
2025-09-22 20:38:20 -0400

    Completed Trivial Methods and Just Completed left() method.

---
+++ Game2048ByMatteoRadcliffe/src/edu/wm/cs/cs301/game2048/SmartPlayer.java
@@ -0,0 +1,147 @@
+package edu.wm.cs.cs301.game2048;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class SmartPlayer {
+
+	public class State implements GameState {
+		private int[][] table = new int[4][4];
+
+		public State(GameState original) {
+			
+		}
+
+		public State() {
+			// TODO Auto-generated constructor stub
+			
+		}
+
+		@Override
+		public int getValue(int xCoordinate, int yCoordinate) {
+			int x = xCoordinate;
+			int y = yCoordinate;
+			
+			return table[y][x];
+		}
+
+		@Override
+		public void setValue(int xCoordinate, int yCoordinate, int value) {
+			table[yCoordinate][xCoordinate] = value;
+
+		}
+
+		@Override
+		public void setEmptyBoard() {
+			for (int i = 0; i < table.length; i++) {
+				for (int j = 0; j < table[i].length; j++) {
+					table[i][j] = 0;
+				}
+			}
+		}
+
+		@Override
+		public boolean addTile() {
+			if (isFull()) {
+				return false;
+			}
+			List<int[]> emptyTiles = new ArrayList<>();
+			for (int i = 0; i < table.length; i++) {
+				for(int j = 0; j < table.length; j++) {
+					if (table[i][j] == 0){
+						emptyTiles.add(new int[] {i,j});
+					}
+				}
+			}
+			Random rand = new Random();
+			int randomIndex = rand.nextInt(emptyTiles.size());
+			int[] chosenSlot = emptyTiles.get(randomIndex);
+			int row = chosenSlot[0];
+			int col = chosenSlot[1];
+			
+			int newValue;
+			int randomNum = rand.nextInt(10);
+			if (randomNum == 4) {
+				newValue = 4;
+			}
+			else {
+				newValue = 2;
+			}
+			table[row][col] = newValue;
+			
+			return true;
+			
+			
+		}
+
+		@Override
+		public boolean isFull() {
+			for (int i = 0; i < table.length; i++) {
+				for(int j = 0; j < table[i].length; j++) {
+					if (table[i][j] == 0) {
+						return false;
+					}
+				}
+			}	
+			return true;
+		}
+
+		@Override
+		public boolean canMerge() {
+			for (int i = 0; i < table.length; i++) {
+				for (int j = 0; j < table[i].length; j++) {
+					if (table[i][j] >= 2048) {
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+
+		@Override
+		public boolean reachedThreshold() {
+			if (isFull()) {
+				return true;
+			}
+			for (int i = 0; i < table.length; i++) {
+				for (int j = 0; j < table.length; j++) {
+					if (table[i][j] >= 2048) {
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+
+		@Override
+		public int left() {
+			/*
+			 * I need to iterate through each row in the table 2d array and in each row move the col(x) position to 0. once this is
+			 * done I can then find the value of that tile, save it in a variable, and add each value of each row to find total. 
+			 */
+			return 0;
+		}
+
+		@Override
+		public int right() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public int down() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public int up() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+	}
+
+
+}
--- Game2048ByMatteoRadcliffe/src/edu/wm/cs/cs301/game2048/State.java
+++ Game2048ByMatteoRadcliffe/src/edu/wm/cs/cs301/game2048/State.java
@@ -1,61 +1,150 @@
 package edu.wm.cs.cs301.game2048;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class State implements GameState {
+	private int[][] table = new int[4][4];
 
 	public State(GameState original) {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public State() {
 		// TODO Auto-generated constructor stub
+		
 	}
 
 	@Override
 	public int getValue(int xCoordinate, int yCoordinate) {
-		// TODO Auto-generated method stub
-		return 0;
+		int x = xCoordinate;
+		int y = yCoordinate;
+		
+		return table[y][x];
 	}
 
 	@Override
 	public void setValue(int xCoordinate, int yCoordinate, int value) {
-		// TODO Auto-generated method stub
+		table[yCoordinate][xCoordinate] = value;
 
 	}
 
 	@Override
 	public void setEmptyBoard() {
-		// TODO Auto-generated method stub
-
+		for (int i = 0; i < table.length; i++) {
+			for (int j = 0; j < table[i].length; j++) {
+				table[i][j] = 0;
+			}
+		}
 	}
 
 	@Override
 	public boolean addTile() {
-		// TODO Auto-generated method stub
-		return false;
+		if (isFull()) {
+			return false;
+		}
+		List<int[]> emptyTiles = new ArrayList<>();
+		for (int i = 0; i < table.length; i++) {
+			for(int j = 0; j < table.length; j++) {
+				if (table[i][j] == 0){
+					emptyTiles.add(new int[] {i,j});
+				}
+			}
+		}
+		
+		Random rand = new Random();
+		int randomIndex = rand.nextInt(emptyTiles.size());
+		int[] chosenSlot = emptyTiles.get(randomIndex);
+		int row = chosenSlot[0];
+		int col = chosenSlot[1];
+		
+		int newValue;
+		int randomNum = rand.nextInt(10);
+		if (randomNum == 4) {
+			newValue = 4;
+		}
+		else {
+			newValue = 2;
+		}
+		table[row][col] = newValue;
+		
+		return true;
 	}
 
 	@Override
 	public boolean isFull() {
-		// TODO Auto-generated method stub
-		return false;
+		for (int i = 0; i < table.length; i++) {
+			for(int j = 0; j < table[i].length; j++) {
+				if (table[i][j] == 0) {
+					return false;
+				}
+			}
+		}	
+		return true;
 	}
 
 	@Override
 	public boolean canMerge() {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < table.length; i++) {
+			for (int j = 0; j < table[i].length; j++) {
+				if (table[i][j] >= 2048) {
+					return true;
+				}
+			}
+		}
 		return false;
 	}
 
 	@Override
 	public boolean reachedThreshold() {
-		// TODO Auto-generated method stub
+		if (isFull()) {
+			return false;
+		}
+		for (int i = 0; i < table.length; i++) {
+			for (int j = 0; j < table.length; j++) {
+				if (table[i][j] >= 2048) {
+					return true;
+				}
+			}
+		}
 		return false;
 	}
 
 	@Override
 	public int left() {
-		// TODO Auto-generated method stub
-		return 0;
+		int points = 0;
+		for (int row = 0; row < 4; row++) {
+			int[] newRow = new int[4];
+			int index = 0;
+			
+			for (int col = 0; col < 4; col++) {
+			if (table[row][col] != 0 ) {
+					newRow[index] = table[row][col];
+					index++;
+				}
+			}
+			table[row] = newRow;
+			
+			for (int col = 0; col < 4; col++) {
+				if (table[row][col] != 0 && table[row][col] == table[row][col + 1]) {
+					table[row][col] *= 2;
+					points += table[row][col];
+					table[row][col + 1] = 0;
+					col++;
+				}
+			}
+			int[] mergedRow = new int [4];
+			index = 0;
+			for (int col = 0; col < 4; col++) {
+				if (table[row][col] != 0) {
+					mergedRow[index] = table[row][col];
+					index++;
+				}
+			}
+			table[row] = mergedRow;
+		}
+		return points;
 	}
 
 	@Override
